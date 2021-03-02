ce3d0fba43932ebee7564e137e8c72a6f40a1c74
Notorio99 <gtovirnac@gmail.com>
2021-03-02 10:12:34 +0100

    "Ya no lo vuelvo a intentar mas"

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 
          $b = 0;
          $html="<table border='1'>";
-         //Hola mundo
+         //Hola mundo 99
             $html=$html."<tr><th>i</th>";
             $html=$html."<th>a</th>";
             $html=$html."<th>b</th></tr>";
