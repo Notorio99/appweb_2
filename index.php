62298b3e648050bc92de9c23bb68f933e861169f
George <gtovirnac@gmail.com>
2021-03-02 10:08:47 +0100

    "ya no falta nada"

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 
          $b = 0;
          $html="<table border='1'>";
-         
+         //Hola mundo
             $html=$html."<tr><th>i</th>";
             $html=$html."<th>a</th>";
             $html=$html."<th>b</th></tr>";
